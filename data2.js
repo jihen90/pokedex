110d16dd66c6db95b4133b5a42fc23373b966f1e
Francisco <francisco.opazo.j@gmail.com>
2021-11-01 21:01:10 -0300

    second commit to overwrite some js and html code

--- data2.js
+++ data2.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     let endpoint = 'https://pokeapi.co/api/v2/pokemon/'
     fetchPokemons();
 
-    $('#nexto').click((e) => {
+    $('#buttonToNext').click((e) => {
         e.preventDefault();
         $('#pokemons').html('');
         fetchPokemons();
@@ -16,21 +16,21 @@ $(document).ready(function () {
             success: function(data) {
                 endpoint = data.next;
                 data.results.forEach(function(pokemon){
-                    let monster = `
+                    let infoPokemon = `
                     <div class="card img-fluid col-4" >
                         <div class="container">
-                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${pokemon.url.split('/')[6]}.png" class="card-img-top" alt="...">
+                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/')[6]}.png" class="card-img-top" alt="...">
                             <div class="card-body text-center">
                                 <h3 class="card-title">${pokemon.name}</h3>
                                 <a href="#" id="${pokemon.name}-${pokemon.url.split('/')[6]}" class="btn btn-outline-primary">¡Quiero saber más de este pokémon</a>
                             </div>
                         </div>
                     </div>`
-                    $('#pokemons').append(monster);
+                    $('#pokemons').append(infoPokemon);
 
                     $(`#${pokemon.name}-${pokemon.url.split('/')[6]}`).click((e) => {
                         e.preventDefault();
-                        $('#diModal').modal('show');
+                        $('#modal').modal('show');
                         $('#pokemonName').html(pokemon.name);
                         $('#pokeName').html(`Nombre:  ${pokemon.name}`);
                         //ajax dentro del evento click
@@ -38,10 +38,10 @@ $(document).ready(function () {
                             url: pokemon.url,
                             dataType: 'json',
                             method: 'GET',
-                            success: function(pokemonsito) {
-                                $('#abilities').html(getAbilities(pokemonsito))
-                                $('#pokeTypes').html(getPokeTypes(pokemonsito))
-                                $('#firstFiveMoves').html(getPokeMoves(pokemonsito))
+                            success: function(forPokemon) {
+                                $('#abilities').html(getAbilities(forPokemon))
+                                $('#pokeTypes').html(getPokeTypes(forPokemon))
+                                $('#firstFiveMoves').html(getPokeMoves(forPokemon))
                             }
                         })
 
@@ -52,7 +52,7 @@ $(document).ready(function () {
     }
 
     function getAbilities(pokemon) {
-        let abi = 'Abilities : '
+        let abi = 'Habilidades : '
         pokemon.abilities.forEach(function(ability){
             abi += ` ${ability.ability.name}`
         })
@@ -61,7 +61,7 @@ $(document).ready(function () {
 
 
     function getPokeTypes(pokemon) {
-        let types = 'Types : '
+        let types = 'Tipo : '
         pokemon.types.forEach(function(type){
             types += ` ${type.type.name}`
         })
@@ -69,10 +69,10 @@ $(document).ready(function () {
     }
 
     function getPokeMoves(pokemon) {
-        let move = 'Moves : '
-        pokemon.moves.forEach(function(movesito, index){
+        let move = 'Movimientos(5) : '
+        pokemon.moves.forEach(function(forMove, index){
             if (index < 5) {
-                move += ` ${movesito.move.name}`
+                move += ` ${forMove.move.name}`
             }
         })
         return move;
